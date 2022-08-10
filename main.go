f7b10498030e15b76f133193e0d5b305eaae7fd2
FilipRutar <fr1789@student.uni-lj.si>
2022-08-10 19:06:31 +0200

    create new endpoint for generating random location

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 type coordinates struct {
 	Latitude  float64 `json:"lat"`
 	Longitude float64 `json:"lng"`
+}
+
+type MetadataResponse struct {
+	Location coordinates `json:"location"`
+	Status   string
 }
 
 var counter int = 0
@@ -22,11 +34,22 @@ var locations = []coordinates{
 	{Latitude: 46.1080212, Longitude: 14.530384},
 	{Latitude: -32.6967123, Longitude: 23.811269},
 	{Latitude: 41.8875707, Longitude: 12.4944658},
+	{Latitude: 46.0856361, Longitude: 14.4226494},
+	{Latitude: 38.9845741, Longitude: -3.9266799},
 }
 
 func main() {
+	//set seed for rand function
+	rand.Seed(time.Now().UnixNano())
+	//try to read .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
 	router.GET("/getLocation", getLocation)
+	router.GET("/getRndLocation", getRndLocation)
 	router.POST("/getDistance", getDistance)
 
 	router.Use(cors.New(cors.Config{
@@ -37,7 +60,6 @@ func main() {
 		//MaxAge: 12 * time.Hour,
 	}))
 	router.Run("0.0.0.0:8080")
-
 }
 
 // getLocation cycles through available locations and responds with one.
@@ -53,6 +75,14 @@ func getLocation(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, locations[counter])
 }
 
+// sends random valid coordinates
+func getRndLocation(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	loc := generateRndLocation()
+	fmt.Println("Sent location: ", loc)
+	c.IndentedJSON(http.StatusOK, loc)
+}
+
 // getDistance reads guess coordinates and calculates distance to the right ones
 // responds with distance in JSON
 func getDistance(c *gin.Context) {
@@ -62,7 +92,7 @@ func getDistance(c *gin.Context) {
 		return
 	}
 
-	fmt.Println("correst location, guess: ", locations[counter], guessLocation)
+	fmt.Println("correst location; guess: ", locations[counter], guessLocation)
 
 	var distance float64 = calcDistance(locations[counter], guessLocation)
 	fmt.Println("distance response: ", fmt.Sprintf(`{"distance": %f}`, distance))
@@ -71,7 +101,6 @@ func getDistance(c *gin.Context) {
 	c.Header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.IndentedJSON(http.StatusOK, fmt.Sprintf(`{"distance": %f}`, distance))
-
 }
 
 // calculates distance between 2 sets of corrdinates using
@@ -86,4 +115,36 @@ func calcDistance(loc_1 coordinates, loc_2 coordinates) float64 {
 	var a float64 = math.Pow(math.Sin(delta_fi/2), 2) + math.Cos(fi_1)*math.Cos(fi_2)*math.Pow(math.Sin(delta_lambda/2), 2)
 	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	return R * c
+}
+
+// generates random coordinates
+func generateRndLocation() coordinates {
+	var status string
+	var location coordinates
+	// alternative for do while, runs until a valid location is generated
+	for next := true; next; next = (status != "OK") {
+		var lat = rand.Float64()*(48-44) + 44
+		var lng = rand.Float64() * 7
+		fmt.Println("generated coordinates; ", lat, lng)
+		location, status = validateLocation(lat, lng)
+		fmt.Println("api response: ", status, " pano location: ", location)
+	}
+	return location
+}
+
+// checks if pano exists near requested location, returns exact location and status code
+func validateLocation(lat float64, lng float64) (coordinates, string) {
+	res, err := http.Get(fmt.Sprintf("https://maps.googleapis.com/maps/api/streetview/metadata?location=%f,%f&key=%s&radius=500", lat, lng, os.Getenv("GMAPS_API")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var response MetadataResponse
+	json.Unmarshal(body, &response)
+	return response.Location, response.Status
 }
