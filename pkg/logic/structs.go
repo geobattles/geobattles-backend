5dd8fcc8dca163dbc4505ebd8db328c549c5360b
FilipRutar <fr1789@student.uni-lj.si>
2022-09-01 17:23:43 +0200

    simplify player attempt results

--- pkg/logic/structs.go
+++ pkg/logic/structs.go
@@ -38,16 +38,17 @@ type Message struct {
 }
 
 type ResponseMsg struct {
-	Status   string                       `json:"status"`
-	Type     string                       `json:"type"`
-	Location *Coordinates                 `json:"location,omitempty"`
-	User     string                       `json:"user,omitempty"`
-	Distance float64                      `json:"distance,omitempty"`
-	Score    int                          `json:"score,omitempty"`
-	Results  map[int]map[string][]Results `json:"results,omitempty"`
-	RoundRes map[string][]Results         `json:"roundRes,omitempty"`
-	Round    int                          `json:"round,omitempty"`
-	Lobby    *Lobby                       `json:"lobby,omitempty"`
+	Status    string                       `json:"status"`
+	Type      string                       `json:"type"`
+	Location  *Coordinates                 `json:"location,omitempty"`
+	User      string                       `json:"user,omitempty"`
+	Results   map[int]map[string][]Results `json:"results,omitempty"`
+	RoundRes  map[string][]Results         `json:"roundRes,omitempty"`
+	PlayerRes Results                      `json:"playerRes,omitempty"`
+	Round     int                          `json:"round,omitempty"`
+	Lobby     *Lobby                       `json:"lobby,omitempty"`
+	//Distance float64                      `json:"distance,omitempty"`
+	//Score    int                          `json:"score,omitempty"`
 }
 
 type LobbyConf struct {
