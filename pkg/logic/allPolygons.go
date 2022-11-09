7182301e7055e6f3b98be34484a57b940f8abbe5
FilipRutar <fr1789@student.uni-lj.si>
2022-11-09 21:45:38 +0100

    temp fix for drawing all polygons

--- pkg/logic/allPolygons.go
+++ pkg/logic/allPolygons.go
@@ -8,7 +8,7 @@ import (
 )
 
 type property struct {
-	Iso_a2 string `json:"ISO_A2"`
+	Iso_a2 string `json:"ISO_A2_EH"`
 }
 
 type feature struct {
@@ -27,18 +27,29 @@ var PolyDB map[string]json.RawMessage
 func readAllPolygons() {
 	PolyDB = make(map[string]json.RawMessage)
 
-	b, _ := os.ReadFile("assets/full/countries.json")
+	b, _ := os.ReadFile("assets/full/countries2.json")
 	if err := json.Unmarshal(b, &importAll); err != nil {
 		fmt.Println(err)
 	}
 	// if feature (country) has a single polygon convert it to a multipolygon for simplicity
 	for _, feature := range importAll.Features {
 		if bytes.Equal([]byte(`"Polygon"`), feature.Geometry["type"]) {
-			PolyDB[feature.Properties.Iso_a2] = append([]byte(`[`), append(feature.Geometry["coordinates"], []byte(`]`)...)...)
+			if PolyDB[feature.Properties.Iso_a2] == nil {
+				PolyDB[feature.Properties.Iso_a2] = append([]byte(`[`), append(feature.Geometry["coordinates"], []byte(`]`)...)...)
+			} else {
+				PolyDB[feature.Properties.Iso_a2] = PolyDB[feature.Properties.Iso_a2][:len(PolyDB[feature.Properties.Iso_a2])-1]
+				PolyDB[feature.Properties.Iso_a2] = append(PolyDB[feature.Properties.Iso_a2], append([]byte(`,`), append(feature.Geometry["coordinates"], []byte(`]`)...)...)...)
+
+			}
+
 		} else {
-			PolyDB[feature.Properties.Iso_a2] = feature.Geometry["coordinates"]
+			if PolyDB[feature.Properties.Iso_a2] == nil {
+				PolyDB[feature.Properties.Iso_a2] = feature.Geometry["coordinates"]
+			} else {
+				PolyDB[feature.Properties.Iso_a2] = PolyDB[feature.Properties.Iso_a2][:len(PolyDB[feature.Properties.Iso_a2])-1]
+				feature.Geometry["coordinates"] = append(feature.Geometry["coordinates"][:0], feature.Geometry["coordinates"][1:]...)
+				PolyDB[feature.Properties.Iso_a2] = append(PolyDB[feature.Properties.Iso_a2], append([]byte(`,`), feature.Geometry["coordinates"]...)...)
+			}
 		}
-
 	}
-
 }
